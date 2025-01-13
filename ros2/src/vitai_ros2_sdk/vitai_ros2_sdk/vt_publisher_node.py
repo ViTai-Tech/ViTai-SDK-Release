34e3164722de42ab1d87f1ac251bc027f9e24c04
sunbin <1247727512@qq.com>
2025-01-13 17:46:47 +0800

    update ros2 支持发布raw img、wrapped img、 depth map、bg depth map、diff depth map、slip state

--- ros2/src/vitai_ros2_sdk/vitai_ros2_sdk/vt_publisher_node.py
+++ ros2/src/vitai_ros2_sdk/vitai_ros2_sdk/vt_publisher_node.py
@@ -3,6 +3,8 @@ from rclpy.node import Node
 from rclpy.qos import QoSProfile
 from cv_bridge import CvBridge
 from sensor_msgs.msg import Image
+from std_msgs.msg import String
+
 from pynput import keyboard
 
 from pyvitaisdk import GF225, VTSDeviceFinder
@@ -11,13 +13,15 @@ import cv2
 
 class VtPublisherNode(Node):
     def __init__(self):
-        super().__init__("vt_publisher_node")
+        super().__init__("VtPublisherNode")
         qos_profile = QoSProfile(depth=10)
         self.raw_img_pub = self.create_publisher(Image, "raw_img", qos_profile)
         self.wrapped_img_pub = self.create_publisher(Image, "wrapped_img", qos_profile)
         self.depth_map_pub = self.create_publisher(Image, "depth_map", qos_profile)
         self.bg_depth_map_pub = self.create_publisher(Image, "bg_depth_map_pub", qos_profile)
         self.diff_depth_map_pub = self.create_publisher(Image, "diff_depth_map", qos_profile)
+
+        self.slip_state_pub = self.create_publisher(String, "slip_state", qos_profile)
 
         self.timer = self.create_timer(0.01, self.timer_callback)
         self.bridge = CvBridge()
@@ -27,31 +31,19 @@ class VtPublisherNode(Node):
         self.scale = 1.5
         self.dsize = [240, 240]
         self.calib_num = 50
-
+        self.model_path = f"/home/sun/code/vitai/SDK-Release/models/2024-11-15-15-52_001.pth"
+        self.device = "cpu"
         self.init_vt()
 
+        self.key = ''
         self.listener = keyboard.Listener(on_press=self.on_press, on_release=self.on_release)
         self.listener.start()
 
 
-    def on_press(self, key):
-        try:
-            key_char = key.char
-            self.get_logger().info(f'Published: "{key_char}"')
-        except AttributeError:
-            pass
-
-
-    def on_release(self, key):
-        if key == keyboard.Key.esc:
-            self.get_logger().info('Exiting...')
-            self.listener.stop()
-            rclpy.shutdown()
-
     def init_vt(self):
         self.finder = VTSDeviceFinder()
         config = self.finder.get_device_by_sn(self.finder.get_sns()[0])
-        self.vt = GF225(config=config, model_path=f"/home/sun/code/vitai/SDK-Release/models/2024-11-15-15-52_001.pth", device="cpu")
+        self.vt = GF225(config=config, model_path=self.model_path, device=self.device)
         self.vt.set_manual_warp_params(self.manual_warp_params, scale=self.scale, dsize=self.dsize)
 
         self.vt.start_backend()
@@ -64,7 +56,6 @@ class VtPublisherNode(Node):
 
         self.publish_image(self.raw_img_pub, raw_frame, encoding="bgr8")
         self.publish_image(self.wrapped_img_pub, wrapped_frame, encoding="bgr8")
-
 
         if self.vt.is_background_depth_init():
             self.vt.recon3d(wrapped_frame)
@@ -82,14 +73,31 @@ class VtPublisherNode(Node):
             flow = self.vt.tracking(wrapped_frame)
             # self.vt.draw_flow(wrapped_frame, flow)
             # print(f"vts.get_markers_offset(): {self.vt.get_markers_offset()}")
-            # print(f"vts.get_marker_vector(): {self.vt.get_marker_vector()}")
+            # print(f"vts.get_marker_vector(): {self.vt.get_marker_vector().shape}")
             # print(f"vts.get_marker_max_offset(): {self.vt.get_marker_max_offset()}")
             # print(f"vts.get_marker_mean_offset(): {self.vt.get_marker_mean_offset()}")
-            # print(f"vts.get_markers(): {self.vt.get_markers()}")
+            # print(f"vts.get_markers(): {self.vt.get_markers().shape}")
+
+
+
         if self.vt.is_calibrate():
             slip_state = self.vt.slip_state()
+            self.publish_msg(self.slip_state_pub, slip_state.name)
+            self.get_logger().info(f'slip_state "{slip_state.name}"')
 
+        if self.key == "e":
+            self.get_logger().info(f'e self.key: "{self.key}"')
+            # 按e开启滑动检测
+            self.vt.enable_slip_detect()
+        elif self.key == "d":
+            self.get_logger().info(f'd self.key: "{self.key}"')
+            # 按d关闭滑动检测
+            self.vt.disable_slip_detect()
+        elif self.key == 'r':
+            self.get_logger().info(f'r self.key: "{self.key}"')
+            self.vt.re_calibrate(self.calib_num)  # 重新标定
 
+        self.key = ''
 
     def publish_image(self, pub, image, encoding):
         # convert to ROS2 image message
@@ -98,7 +106,27 @@ class VtPublisherNode(Node):
         pub.publish(ros_img)
 
 
+    def publish_msg(self, pub, data):
+        msg = String()
+        msg.data = data
+        pub.publish(msg)
 
+
+    def on_press(self, key):
+        try:
+            self.key = key.char
+            self.get_logger().info(f'Published: "{self.key}"')
+        except AttributeError:
+            pass
+
+
+    def on_release(self, key):
+        if key == keyboard.Key.esc:
+            self.get_logger().info('Exiting...')
+            self.listener.stop()
+            rclpy.shutdown()
+            self.vt.release()
+            self.vt.stop_backend()
 
 def main(args=None):
     rclpy.init(args=args)
--- ros2/src/vitai_ros2_sdk/vitai_ros2_sdk/vt_subscriber_node.py
+++ ros2/src/vitai_ros2_sdk/vitai_ros2_sdk/vt_subscriber_node.py
@@ -1,29 +1,34 @@
 import rclpy
 from rclpy.node import Node
+from rclpy.qos import QoSProfile
 from sensor_msgs.msg import Image
+from std_msgs.msg import String
 from cv_bridge import CvBridge
 import cv2
 
-class ImageSubscriber(Node):
+class VTSubscriberNode(Node):
     def __init__(self):
-        super().__init__('image_subscriber')
-        self.subscription = self.create_subscription(
-            Image,
-            'depth_map',
-            self.listener_callback,
-            10)
-        self.subscription  # prevent unused variable warning
+        super().__init__('VTSubscriberNode')
+        qos_profile = QoSProfile(depth=10)
+        self.depth_map_sub = self.create_subscription(Image, 'depth_map', self.depth_map_callback, qos_profile)
+
+        self.slip_state_sub = self.create_subscription(String, 'slip_state', self.slip_state_callback, qos_profile)
+
         self.bridge = CvBridge()
 
-    def listener_callback(self, msg):
+    def depth_map_callback(self, msg):
         cv_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding='32FC1')
-        self.get_logger().info('Subscribe image')
+        # self.get_logger().info('Subscribe image')
         cv2.imshow('Received Image', cv_image)
         cv2.waitKey(1)
 
+
+    def slip_state_callback(self, msg):
+        self.get_logger().info('Slip State: {}'.format(msg.data))
+
 def main(args=None):
     rclpy.init(args=args)
-    image_subscriber = ImageSubscriber()
+    image_subscriber = VTSubscriberNode()
     rclpy.spin(image_subscriber)
     rclpy.shutdown()
 
