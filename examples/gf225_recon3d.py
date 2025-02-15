b414ee9fdbc8ef2ce08b7fe7022499446413952a
sunbin <1247727512@qq.com>
2025-02-15 09:56:34 +0800

    add flag save

--- examples/gf225_recon3d.py
+++ examples/gf225_recon3d.py
@@ -6,7 +6,7 @@ Description  : Example:深度估计
 from datetime import datetime
 import cv2
 import numpy as np
-
+import os
 from pyvitaisdk import GF225, VTSDeviceFinder
 from utils import get_project_root, put_text_to_image, create_folder
 
@@ -30,6 +30,7 @@ def main():
     gf225.start_backend()
     bg = gf225.get_warped_frame()
     gf225.set_background(bg)
+    save = False
     while 1:
         frame = gf225.get_warped_frame()
         if gf225.is_background_init():
@@ -43,10 +44,10 @@ def main():
             put_text_to_image(frame_copy, str(np.mean(depth_map)), (10,60))
             cv2.imshow(f"warped_frame", frame_copy)
 
-            # formatted_now = datetime.now().strftime("%Y_%m_%d_%H_%M_%S_%f")
-            # cv2.imwrite(os.path.join(folder, f"frame_{formatted_now}.png"), frame)
-            # cv2.imwrite(os.path.join(folder, f"depth_map_{formatted_now}.png"), depth_map)
-            # print(f'dz {np.max(depth_map)}')
+            if save:
+                formatted_now = datetime.now().strftime("%Y_%m_%d_%H_%M_%S_%f")
+                cv2.imwrite(os.path.join(folder, f"frame_{formatted_now}.png"), frame)
+                cv2.imwrite(os.path.join(folder, f"depth_map_{formatted_now}.png"), depth_map)
 
         key = cv2.waitKey(1) & 0xFF
         if key == 27 or key == ord("q"):
